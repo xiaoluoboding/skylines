dfa8633099a20dcd65e208fb1effe8649e9f3849
xiaoluoboding <xiaoluoboding@gmail.com>
2021-03-04 01:42:57 +0800

    feat: built on top of vite2 & tailwindcss2

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <skyline />
+</template>
+
+<script setup>
+import Skyline from './components/Skyline.vue'
+</script>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+}
+
+body {
+  background-image: radial-gradient(50% 100%,#180526 0,#040e21 100%);
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
